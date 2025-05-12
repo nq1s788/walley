b28c27af04b69064259185ca93faeda65a6ec7b9
nq1s788 <dchsk@inbox.ru>
2025-05-12 20:40:59 +0300

    мейн добавиоа

--- .idea/DB.java
+++
@@ -1,5 +0,0 @@
-public class DB {
-    public static void main(String[] args) {
-        System.out.printf(1);
-    }
-}
---
+++ src/main/DB.java
@@ -0,0 +1,7 @@
+package main;
+
+public class DB {
+    public static void print1() {
+        System.out.printf("дб дергается");
+    }
+}
