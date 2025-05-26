4cdd2228243224efb4f7cef174219583cb158433
MarkovaAnna <ann.markovva@gmail.com>
2025-05-26 22:19:07 +0300

    нужно насоздавать репозиториев. добавила session

--- src/main/java/com/walley/walley/controllers/MainController.java
+++ src/main/java/com/walley/walley/controllers/MainController.java
@@ -1,15 +1,19 @@
 package com.walley.walley.controllers;
 
 import com.walley.walley.models.MyUser;
+import com.walley.walley.models.UserSetting;
+import com.walley.walley.models.UserStat;
+import com.walley.walley.models.UserTimer;
 import com.walley.walley.repo.MyUserRepository;
 import com.walley.walley.services.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-//import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 
 
 //Контроллер отслеживает все переходы пользователя между страницами
@@ -36,6 +40,7 @@ public class MainController {
     public String handleSubmit(@RequestParam String action,
                                @RequestParam String email,
                                @RequestParam String password,
+                               HttpSession session,
                                Model model) {
         if ("register".equals(action)) {
             if (service.userExists(email)) {
@@ -43,7 +48,15 @@ public class MainController {
                 return "index"; // Вернуться на страницу с ошибкой
             }
             MyUser user = new MyUser(email, password);
+            UserSetting userSetting = new UserSetting(email);
+            UserStat userStat = new UserStat(email);
+            UserTimer userTimer = new UserTimer(email);
+            //сохраняем всех по репозиториям
             userRepository.save(user);
+            session.setAttribute("user", user);
+            session.setAttribute("user", userSetting);
+            session.setAttribute("user", userStat);
+            session.setAttribute("user", userTimer);
 
             return "garden"; // Перенаправление после успешной регистрации
         } else if ("login".equals(action)) {
@@ -51,6 +64,7 @@ public class MainController {
                 model.addAttribute("error", "Неверный логин или пароль");
                 return "index"; // Вернуться на страницу с ошибкой
             }
+            session.setAttribute("user", userRepository.findByEmail(email));
             return "garden"; // Перенаправление на страницу после входа
         }
         return "index"; // На всякий случай
