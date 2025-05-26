170ceefaa489aa4e9c6e6ef2424e7ad010893371
nq1s788 <dchsk@inbox.ru>
2025-05-26 23:22:38 +0300

    Notes, Threads, Walls репозитории добавлены

---
+++ src/main/java/com/walley/walley/repo/WallsRepository.java
@@ -0,0 +1,11 @@
+package com.walley.walley.repo;
+
+import com.walley.walley.models.Walls;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface WallsRepository extends CrudRepository<Walls, Long> {}
