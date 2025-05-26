c0884a9563a8444ed8b3250534cdaa176f8f792d
nq1s788 <dchsk@inbox.ru>
2025-05-26 22:44:26 +0300

    репозитории юзера

--- src/main/java/com/walley/walley/repo/UserSettingRepository.java
+++ src/main/java/com/walley/walley/repo/UserSettingRepository.java
@@ -1,4 +1,13 @@
 package com.walley.walley.repo;
 
-public interface UserSettingRepository {
+import com.walley.walley.models.UserSetting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserSettingRepository extends CrudRepository<UserSetting, String> {
+    UserSetting findByEmail(String email);
 }
--- src/main/java/com/walley/walley/repo/UserTimerRepository.java
+++ src/main/java/com/walley/walley/repo/UserTimerRepository.java
@@ -1,4 +1,13 @@
 package com.walley.walley.repo;
 
-public interface UserTimerRepository {
+import com.walley.walley.models.UserTimer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserTimerRepository extends CrudRepository<UserTimer, String> {
+    UserTimer findByEmail(String email);
 }
