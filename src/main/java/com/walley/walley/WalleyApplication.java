2ad8610a256117ed33cf6ea1d0a055f845549853
nq1s788 <dchsk@inbox.ru>
2025-05-26 15:14:39 +0300

    не видит MyUser как managed type

--- src/main/java/com/walley/walley/WalleyApplication.java
+++ src/main/java/com/walley/walley/WalleyApplication.java
@@ -1,17 +1,9 @@
 package com.walley.walley;
 
-import com.walley.walley.controllers.MainController;
-
-import com.walley.walley.repo.MyUserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 @EntityScan("com.walley.walley.models")
@@ -19,6 +11,7 @@ import java.nio.charset.StandardCharsets;
 public class WalleyApplication {
 
 	public static void main(String[] args) {
+		///пожалуйста
 		SpringApplication.run(WalleyApplication.class, args);
 	}
 
