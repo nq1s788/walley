a4577016876a8627142b9980bb5f694dc5048110
MarkovaAnna <ann.markovva@gmail.com>
2025-05-26 23:09:36 +0300

    garden и folders контроллеры

--- src/main/java/com/walley/walley/controllers/FoldersController.java
+++ src/main/java/com/walley/walley/controllers/FoldersController.java
@@ -1,4 +1,39 @@
 package com.walley.walley.controllers;
 
+import com.walley.walley.repo.MyUserRepository;
+import com.walley.walley.services.AppService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class FoldersController {
+    @Autowired
+    private AppService service;
+    @Autowired
+    private final MyUserRepository userRepository;
+
+    public FoldersController(MyUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @GetMapping("/folders")
+    public String gardenMain(HttpSession session, Model model) {
+        return "folders";
+    }
+
+    @PostMapping("/folders")
+    public String goToNextPages(@RequestParam String action,
+                                HttpSession session,
+                                Model model) {
+        if ("garden".equals(action)) {
+            return "garden";
+        }
+        return "folders";
+    }
 }
--- src/main/java/com/walley/walley/controllers/GardenController.java
+++ src/main/java/com/walley/walley/controllers/GardenController.java
@@ -2,6 +2,7 @@ package com.walley.walley.controllers;
 
 import com.walley.walley.repo.MyUserRepository;
 import com.walley.walley.services.AppService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,19 +14,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GardenController {
     @Autowired
     private AppService service;
+    @Autowired
     private final MyUserRepository userRepository;
 
     public GardenController(MyUserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+
     @GetMapping("/garden")
-    public String gardenMain(Model model) {
+    public String gardenMain(HttpSession session, Model model) {
         return "garden";
     }
     @PostMapping("/garden")
     public String goToNextPages(@RequestParam String action,
-                             Model model) {
+                                HttpSession session,
+                                Model model) {
         if ("settings".equals(action)) {
             return "settings";
         }
