d4943b8025187d7ba0003f83595e2b9ba17800f6
MarkovaAnna <ann.markovva@gmail.com>
2025-06-10 00:11:08 +0300

    прописали boardController

--- src/main/java/com/walley/walley/controllers/BoardController.java
+++ src/main/java/com/walley/walley/controllers/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -47,9 +48,78 @@ public class BoardController {
         return "board/{id}";
     }
     @PostMapping("/board/{id}")
-    public String boardMain(@PathVariable Long id, HttpSession session, Model model) {
-        return "board/{id}";
+    public String boardMain(@PathVariable Long id, HttpSession session, Model model,
+                            @RequestParam(required = false) String action,
+                            @RequestParam(required = false) Walls wall,
+                            @RequestParam(required = false) List<Notes> notes,
+                            @RequestParam(required = false) List<Threads> threads) {
+        if ("update".equals(action)) {
+            MyUser user = (MyUser) session.getAttribute("user");
+            Walls old_wall = (Walls) session.getAttribute("wall");
+            old_wall.setTitle(wall.getTitle());
+            old_wall.setBackground(wall.getBackground());
+            old_wall.setFont(wall.getFont());
+            wallsRepository.save(old_wall);
+
+            List<Notes> old_notes = (List<Notes>) session.getAttribute("notes");
+            for (Notes old_note : old_notes) {
+                Boolean is_deleted = true;
+                for (Notes new_note : notes) {
+                    if (old_note.getId().equals(new_note.getId())) {
+                        is_deleted = false;
+                        old_note.setColor(new_note.getColor());
+                        old_note.setContent(new_note.getContent());
+                        old_note.setDataX(new_note.getDataX());
+                        old_note.setDataY(new_note.getDataY());
+                        old_note.setIstime(new_note.isIstime());
+                        old_note.setHeadline(new_note.isHeadline());
+                        notesRepository.save(old_note);
+                    }
+                }
+                if (is_deleted) {
+                    notesRepository.deleteById(old_note.getId());
+                }
+            }
+            for (Notes new_note : notes) {
+                Boolean is_new = true;
+                for (Notes old_note : old_notes) {
+                    if (new_note.getId().equals(old_note.getId())) {
+                        is_new = false;
+                    }
+                }
+                if (is_new) {
+                    notesRepository.save(new_note);
+                }
+            }
+
+            List<Threads> old_threads = (List<Threads>) session.getAttribute("threads");
+            for (Threads old_thread : old_threads) {
+                Boolean is_deleted = true;
+                for (Threads new_thread : threads) {
+                    if (old_thread.getId().equals(new_thread.getId())) {
+                        is_deleted = false;
+                    }
+                }
+                if (is_deleted) {
+                    threadsRepository.deleteById(old_thread.getId());
+                }
+            }
+            for (Threads new_thread : threads) {
+                Boolean is_new = true;
+                for (Threads old_thread : old_threads) {
+                    if (new_thread.getId().equals(old_thread.getId())) {
+                        is_new = false;
+                    }
+                }
+                if (is_new) {
+                    threadsRepository.save(new_thread);
+                }
+            }
+        }
+        return "board";
     }
+
+
 
 
 
